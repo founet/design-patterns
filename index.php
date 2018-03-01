faad343064c7b26e0f23626f58df9ea3fac6ebf5
Amadou BAH <abah@maltem.com>
2018-03-01 09:16:35 +0100

    abstract factory pattern

--- index.php
+++ index.php
@@ -5,12 +5,22 @@ require __DIR__ . '/vendor/autoload.php';
 
 
 $highBudgetMachineFactory = new \patterns\Factory\HighBudgetMachineFactory();
+$lowMachineFactory = new \patterns\Factory\LowBudgetMachineFactory();
 
-$machineFactory = new \patterns\Factory\MachineFactory($highBudgetMachineFactory);
+$machineFactory1 = new \patterns\Factory\MachineFactory($highBudgetMachineFactory);
+$machineFactory2 = new \patterns\Factory\MachineFactory($lowMachineFactory);
 
-$machine = $machineFactory->createMachine();
+$machine1 = $machineFactory1->createMachine();
+$machine2 = $machineFactory2->createMachine();
 
-dump($machine->getMonitor()->displayPicture());
+$shop = [];
+
+$shop [] = $machine1;
+$shop [] = $machine2;
+
+foreach ($shop as $machine) {
+    $machine->getProcessor()->performOperation();
+}
 
 
 
--- src/Factory/LowBudgetMachineFactory.php
+++ src/Factory/LowBudgetMachineFactory.php
@@ -12,9 +12,9 @@ namespace patterns\Factory;
 use patterns\Entity\CheapHardDisk;
 use patterns\Entity\CheapProcessor;
 use patterns\Entity\LowResolutionMonitor;
-use patterns\HardDiskInterface;
-use patterns\MonitorInterface;
-use patterns\ProcessorInterface;
+use patterns\Interfaces\HardDiskInterface;
+use patterns\Interfaces\MonitorInterface;
+use patterns\Interfaces\ProcessorInterface;
 
 class LowBudgetMachineFactory implements MachineFactoryInterface
 {
